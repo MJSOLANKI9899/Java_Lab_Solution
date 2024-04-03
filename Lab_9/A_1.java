487abcabbc30fc8eadc7ab0ba45e9fdca24b75b8
MJSOLANKI9899 <165764776+MJSOLANKI9899@users.noreply.github.com>
2024-04-03 13:18:21 +0530

    Add files via upload

--- Lab_9/A_1.java
+++ Lab_9/A_1.java
@@ -1,1 +1,36 @@
-
+class GM implements Runnable{
+    public void run(){
+        while(true){
+            System.out.println("Goog Morning");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class GN implements Runnable{
+    public void run(){
+        while(true){
+            System.out.println("Goog Night");
+            try{
+                Thread.sleep(3000);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+public class A_1 {
+    public static void main(String[] args) {
+        GM g1=new GM();
+        GN g2=new GN();
+        Thread t1=new Thread(g1);
+        Thread t2=new Thread(g2);
+        t1.start();
+        t2.start();
+    }
+}
